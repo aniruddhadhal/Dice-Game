7ef2a72dac8d53f8efa7148a9e258f62bb5e10a0
Sandip Pal <46566422+Sandip99@users.noreply.github.com>
2020-07-21 06:18:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomImage1);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomImage2);
+if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩️";
+}
+else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩️ Player 1 Wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
